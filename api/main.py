8a307b6b686ba444c333a1ec029ba2451f0e29ca
Heytor Víctor <heytorvas@gmail.com>
2022-12-03 18:08:58 -0300

    chore: enable cors on api

--- api/main.py
+++ api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.salary import router as salary_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(salary_router, prefix='/api/v1/salary', tags=['salary'])
 
