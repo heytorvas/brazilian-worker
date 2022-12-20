1f53563fda36bce62c652345b228cfdb0c28f80a
Heytor Víctor <heytor@heytor.dev>
2022-12-20 13:34:01 -0300

    refactor: change attach for attachment (#44)

--- api/scripts/pj_scraping.py
+++ api/scripts/pj_scraping.py
@@ -1,29 +1,38 @@
-import requests, re
+import re
+
+import requests
 from bs4 import BeautifulSoup
+
 
 def _formatter_currency(value):
     return float(value.replace(".", "").replace(",", "."))
+
 
 def _formatter_value_from_table(row):
     values = [_formatter_currency(item) for item in re.findall(r'[\d]+[.,\d]+', row)]
     return {
         "min": 0 if len(values) == 1 else values[0],
-        "max": values[0] if len(values) == 1 else values[1]
+        "max": values[0] if len(values) == 1 else values[1],
     }
+
 
 def _formatter_others_values(value):
     string = re.findall(r'[\d]+[.,\d]+', value)
     return float(string[0].replace(".", "").replace(",", ".")) if len(string) > 0 else 0
 
+
 def get_html_body():
-    r = requests.get('https://www.contabilizei.com.br/contabilidade-online/tabela-simples-nacional-completa/').text
+    r = requests.get(
+        'https://www.contabilizei.com.br/contabilidade-online/tabela-simples-nacional-completa/'
+    ).text
     soup = BeautifulSoup(r, 'html.parser')
     body = soup.find('body')
     return body.find_all('figure', {'class': 'wp-block-table'})
 
-def get_all_attaches():
+
+def get_all_attachments():
     tables = get_html_body()
-    attaches = ['I', 'II', 'III', 'IV', 'V']
+    attachments = ['I', 'II', 'III', 'IV', 'V']
     response = {}
     for j in range(5):
         rows = tables[j].find_all('tr')
@@ -33,10 +42,10 @@ def get_all_attaches():
             tds = rows[i].find_all('td')
             row = {
                 "percent": _formatter_others_values(tds[1].text),
-                "deduction": _formatter_others_values(tds[2].text)
+                "deduction": _formatter_others_values(tds[2].text),
             }
             row.update(_formatter_value_from_table(tds[0].text))
             aux.append(row)
-        
-        response.update({attaches[j]: aux})
+
+        response.update({attachments[j]: aux})
     return response
--- api/services/pj.py
+++ api/services/pj.py
@@ -5,9 +5,9 @@ from models.pj import PJ
 PJ_DATA = json.load(open('data/pj.json', 'r'))
 
 
-def _find_percentage_and_deduction(attach, salary):
+def _find_percentage_and_deduction(attachment, salary):
     salary = _calculate_salary_per_year(salary)
-    for index in PJ_DATA[attach]:
+    for index in PJ_DATA[attachment]:
         if index['min'] <= salary <= index['max']:
             return index['percent'], index['deduction']
 
@@ -16,11 +16,13 @@ def _calculate_salary_per_year(raw):
     return raw * 12
 
 
-def _get_tax_value(attach, raw):
-    percentage, deduction = _find_percentage_and_deduction(attach, raw)
+def _get_tax_value(attachment, raw):
+    percentage, deduction = _find_percentage_and_deduction(attachment, raw)
     return round(raw * (percentage / 100) - (deduction / 12), 2)
 
 
 def calculate_pj_salary(input):
-    tax = _get_tax_value(input.attach, input.raw)
-    return PJ(attach=input.attach, raw=input.raw, tax=tax, total=input.raw - tax)
+    tax = _get_tax_value(input.attachment, input.raw)
+    return PJ(
+        attachment=input.attachment, raw=input.raw, tax=tax, total=input.raw - tax
+    )
