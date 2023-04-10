6f513417cc66740d267a55be81f248850a7f75b7
Heytor Víctor <heytorvas@gmail.com>
2023-04-09 21:51:46 -0300

    refactor: fix typo on daily_hours word

--- api/tests/e2e/test_v1_hour.py
+++ api/tests/e2e/test_v1_hour.py
@@ -13,7 +13,7 @@ class TestIntegrationHour:
         "Content-Type": "application/json",
     }
 
-    def test_calculate_salary_hour_without_daily_hour(self):
+    def test_calculate_salary_hour_without_daily_hours(self):
         body = {"raw": 3000, "weekly_hours": 40}
         response = json.loads(
             client.post(self.URL, headers=self.HEADERS, json=body).text
@@ -21,20 +21,20 @@ class TestIntegrationHour:
 
         assert response["raw"] == 3000
         assert response["weekly_hours"] == 40
-        assert response["daily_hour"] is None
+        assert response["daily_hours"] is None
         assert response["hour_value"] == 15
         assert response["day_value"] is None
         assert response["extra_hour_value"] == 22.5
 
-    def test_calculate_salary_hour_with_daily_hour(self):
-        body = {"raw": 3000, "weekly_hours": 40, "daily_hour": 8}
+    def test_calculate_salary_hour_with_daily_hours(self):
+        body = {"raw": 3000, "weekly_hours": 40, "daily_hours": 8}
         response = json.loads(
             client.post(self.URL, headers=self.HEADERS, json=body).text
         )
 
         assert response["raw"] == 3000
         assert response["weekly_hours"] == 40
-        assert response["daily_hour"] == 8
+        assert response["daily_hours"] == 8
         assert response["hour_value"] == 15
         assert response["day_value"] == 120
         assert response["extra_hour_value"] == 22.5
--- api/tests/unit/test_hour.py
+++ api/tests/unit/test_hour.py
@@ -8,12 +8,12 @@ class TestHourBaseModel:
         hour = HourBase(raw=5000, weekly_hours=40)
         assert hour.raw == 5000
         assert hour.weekly_hours == 40
-        assert hour.daily_hour is None
+        assert hour.daily_hours is None
 
-        hour = HourBase(raw="5000", weekly_hours="40", daily_hour="8")
+        hour = HourBase(raw="5000", weekly_hours="40", daily_hours="8")
         assert hour.raw == 5000
         assert hour.weekly_hours == 40
-        assert hour.daily_hour == 8
+        assert hour.daily_hours == 8
 
     def test_create_invalid_hour_base(self):
         with pytest.raises(ValueError):
@@ -21,12 +21,12 @@ class TestHourBaseModel:
         with pytest.raises(ValueError):
             HourBase(raw=5000, weekly_hours="opa")
         with pytest.raises(ValueError):
-            HourBase(raw=5000, weekly_hours=40, daily_hour="opa")
+            HourBase(raw=5000, weekly_hours=40, daily_hours="opa")
 
 
 class TestHourModel:
-    def test_check_without_daily_hour_should_return_none(self):
+    def test_check_without_daily_hours_should_return_none(self):
         base = HourBase(raw=5000, weekly_hours=40)
         hour = calculate_hour(base)
-        assert base.daily_hour is None
+        assert base.daily_hours is None
         assert hour.day_value is None
